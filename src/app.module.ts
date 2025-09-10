04eb4cc62c96551c9149f4ec456f846d6db9f143
Juan Jose <juanjose10312@gmail.com>
2025-09-10 08:43:38 -0700

    fix: add missing commas in module providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
